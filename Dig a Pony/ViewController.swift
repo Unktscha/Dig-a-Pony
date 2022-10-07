e0d5b2025c99e19ef114e82bce95e05b3e1a3d08
Martin Rose <unktscha@icloud.com>
2022-10-07 18:46:31 +0200

    Initial Commit

---
+++ Dig a Pony/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dig a Pony
+//
+//  Created by Martin Rose on 07.10.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
